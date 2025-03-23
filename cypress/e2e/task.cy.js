94255b96093e32a238f5e5a67c07fdfcb9a626e3
Habib0 <habibullah438@gmail.com>
2025-03-23 21:29:00 +0500

    new changes add in tasks

--- cypress/e2e/task.cy.js
+++ cypress/e2e/task.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
-describe('template spec', () => {
-  it('passes', () => {
+describe('daily task added - covis', () => {
+  it('login', () => {
     cy.clearAllCookies()
     cy.visit('/login')
     // cy.get('[href="/login"]').should('be.visible').click({force:true})
@@ -15,17 +15,17 @@ describe('template spec', () => {
     cy.get('[name="title"]').type('meeting with nick')
     cy.get('[placeholder="Add Task Description"]').type('Availability for Nick after office hours')
     cy.get('[placeholder="00:00"]').clear().type('05')
-    // cy.get('[type="submit"]').click({force:true})
-    // cy.contains('Task Added Successfully').should('exist')
-    // cy.contains('Task Added Successfully').should('not.exist')
+    cy.get('[type="submit"]').click({force:true})
+    cy.contains('Task Added Successfully').should('exist')
+    cy.contains('Task Added Successfully').should('not.exist')
   })
-  // it("day planner",()=>{
-  //   cy.contains('button', "Add Task").click({force:true})
-  //   // cy.get('[name="title"]').type('meeting with nick')
-  //   cy.get('[placeholder="Add Task Description"]').type('Verify Daily Reminder services')
-  //   cy.get('[placeholder="00:00"]').clear().type('01')
-  //   cy.get('[type="submit"]').click({force:true})
-  //   cy.contains('Task Added Successfully').should('exist')
-  //   cy.contains('Task Added Successfully').should('not.exist')
-  // })
+  it("day planner",()=>{
+    cy.contains('button', "Add Task").click({force:true})
+    // cy.get('[name="title"]').type('meeting with nick')
+    cy.get('[placeholder="Add Task Description"]').type('Verify Daily Reminder services')
+    cy.get('[placeholder="00:00"]').clear().type('01')
+    cy.get('[type="submit"]').click({force:true})
+    cy.contains('Task Added Successfully').should('exist')
+    cy.contains('Task Added Successfully').should('not.exist')
+  })
 })
